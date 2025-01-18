d3bdf3ccec71cf3c0de2b121a290455e4de84913
Varad Prabhu <varadprabhu111@gmail.com>
2025-01-18 22:58:46 +0530

    fix: 🐛 landing styling and verbosity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
-import { Features, Hero, Navbar, Steps } from "@/components/pages/landing";
-import { Pricing } from "@/components/pages/landing/client";
+import { Features, Hero, Navbar, Steps } from "@/components/pages/landing"
+import { Pricing } from "@/components/pages/landing/client"
 
 export default function Home() {
-  return (
-    <div className="relative overflow-clip min-h-screen">
-      <div
-        className="w-full h-svh absolute top-0 right-0 -z-10 translate-x-1/2 -translate-y-1/4 pointer-events-none bg-transparent"
-        style={{
-          backgroundImage: "radial-gradient(#1D0199 , rgba(48,1,255,0) 70%)",
-        }}
-      />
-      <Navbar />
-      <Hero />
-      <Features />
-      <Steps />
-      <Pricing />
-    </div>
-  );
+	return (
+		<div className="relative min-h-screen overflow-x-clip">
+			<div
+				className="pointer-events-none absolute right-0 top-0 -z-10 h-svh w-full -translate-y-1/4 translate-x-1/2 bg-transparent"
+				style={{
+					backgroundImage:
+						"radial-gradient(#1D0199 , rgba(48,1,255,0) 70%)",
+				}}
+			/>
+			<div className="container mx-auto">
+				<Navbar />
+				<Hero />
+				<Features />
+				<Steps />
+				<Pricing />
+			</div>
+		</div>
+	)
 }
--- src/lib/data/landing.ts
+++ src/lib/data/landing.ts
@@ -1,144 +1,129 @@
 export type TNavLink = {
-  title: string;
-  href: string;
-};
+	title: string
+	href: string
+}
 export const navLinks: TNavLink[] = [
-  {
-    title: "Home",
-    href: "/",
-  },
-  {
-    title: "Features",
-    href: "/features",
-  },
-  {
-    title: "Docs",
-    href: "/docs",
-  },
-  {
-    title: "Demo",
-    href: "/demo",
-  },
-  {
-    title: "About",
-    href: "/about",
-  },
-];
+	{ title: "Home", href: "/" },
+	{ title: "Features", href: "/features" },
+	{ title: "Docs", href: "/docs" },
+	{ title: "Demo", href: "/demo" },
+	{ title: "About", href: "/about" },
+]
 
 export type FeatureCardProps = {
-  img: string;
-  title: string;
-  description: string;
-};
+	img: string
+	title: string
+	description: string
+}
+const commonImg = "/landing/image.png"
 export const featureCardData: FeatureCardProps[] = [
-  {
-    img: "/landing/image.png",
-    title: "Auto-Scan Repository",
-    description:
-      "Our tool automatically scans your GitHub repository to detect the tech stack, dependencies, and project configuration. It ensures every detail needed for thorough documentation is captured seamlessly.",
-  },
-  {
-    img: "/landing/image.png",
-    title: "AI-Powered Documentation ",
-    description:
-      "Say goodbye to manual documentation! Our AI leverages advanced algorithms to generate insightful README files that highlight the project’s purpose, features, and structure, all tailored to industry standards.",
-  },
-  {
-    img: "/landing/image.png",
-    title: "Webpage Screenshot Integration",
-    description:
-      "Our tool automatically captures screenshots of relevant project screens, adding visual guidance to the README. These images enhance the documentation, making it more intuitive for users and contributors.",
-  },
-];
+	{
+		img: commonImg,
+		title: "Auto-Scan Repository",
+		description:
+			"Automatically scans your GitHub repository to detect the tech stack, dependencies, and project configuration.",
+	},
+	{
+		img: commonImg,
+		title: "AI-Powered Documentation",
+		description:
+			"Generates insightful README files highlighting the project’s purpose, features, and structure.",
+	},
+	{
+		img: commonImg,
+		title: "Webpage Screenshot Integration",
+		description:
+			"Captures relevant project screenshots to enhance README documentation.",
+	},
+]
 
-export type StepCardProps = {
-  title: string;
-  steps: { title: string; description: string }[];
-};
+export type Step = { title: string; description: string }
+export type StepCardProps = { title: string; steps: Step[] }
 
 export const stepsData: StepCardProps[] = [
-  {
-    title: "Connect to GitHub Repository",
-    steps: [
-      {
-        title: "Accessing the Codebase",
-        description:
-          "The journey begins by linking the specific GitHub repository to our tool. This connection allows the tool to access the codebase, analyze the project, and prepare for detailed documentation generation. ",
-      },
-      {
-        title: "Documentation begins with understanding the code",
-        description:
-          "By scanning the repository, our tool gains essential insights, such as the tech stack (frameworks, libraries, etc.), dependencies, directory structure, and core files. This preliminary overview helps build a foundation for effective README content.",
-      },
-    ],
-  },
-  {
-    title: "Downloading and Analyzing the Codebase",
-    steps: [
-      {
-        title: "Fetching and Structuring Data",
-        description:
-          "Once the repository is linked, our tool automatically downloads and organizes the code files locally to facilitate in-depth analysis. The download includes all necessary project files and directories—each representing different aspects of the codebase.",
-      },
-      {
-        title: "Code Parsing and Analysis",
-        description:
-          "The tool parses through the downloaded code, identifying key elements like class definitions, functions, modules, and their relationships. This automated analysis is essential in establishing a comprehensive understanding of the code’s structure, functionality, and dependencies.",
-      },
-      {
-        title:
-          "Every line of code has a story to tell, and it's our job to make it clear.",
-        description:
-          "By identifying code components, the tool prepares to offer accurate summaries of the project’s purpose, core functionality, and technical details in an accessible format for developers and non-developers alike.",
-      },
-    ],
-  },
-  {
-    title: "Graph Creation for Visual Representation",
-    steps: [
-      {
-        title: "Generating Graphs and Visualizations",
-        description:
-          "Visual representation of code is a powerful way to communicate complex project structures and dependencies. Using the information gathered, the tool creates graphs illustrating the relationships between various files, modules, and functions.",
-      },
-      {
-        title: "Dependency and Hierarchical Graphs",
-        description:
-          "The tool generates dependency graphs that highlight how different components of the codebase interact, allowing users to see the project’s architectural hierarchy at a glance. These graphs provide clarity on dependencies and interconnected files, which simplifies understanding and troubleshooting.",
-      },
-      {
-        title: "Interactive and Customizable Visuals",
-        description:
-          "Users can customize and interact with these graphs to delve deeper into specific areas, making it easier to comprehend intricate project structures. This feature is especially useful for large repositories with multiple layers of complexity.",
-      },
-      {
-        title: "A picture is worth a thousand lines of code",
-        description:
-          "Visuals enable developers to quickly grasp the overall structure and key connections in the project, bridging the gap between abstract code and functional understanding.",
-      },
-    ],
-  },
-  {
-    title: "Summarizing the Code and Its Core Functions",
-    steps: [
-      {
-        title: "Extracting Key Functions and Purpose",
-        description:
-          "Using AI-powered algorithms, our tool analyzes and summarizes each function, module, and class, delivering concise explanations that clarify their purpose and role in the project. This automated summary goes beyond simply listing functions; it provides valuable context for how they interact and contribute to the project's overall goal.",
-      },
-      {
-        title: "Automated Summary for Core Project Aspects",
-        description:
-          "For functions, the tool extracts names, parameters, return types, and descriptive comments, if available. It then summarizes this information in an accessible format, ensuring the generated README file includes detailed yet concise explanations of core project elements.",
-      },
-      {
-        title: "Integration with README Components",
-        description: `These summaries are then organized within the README in relevant sections—such as "Features," "Usage," "Dependencies," or "Configuration"—according to the project's structure. This integration keeps all essential information in one place, providing users with a cohesive understanding of the project.`,
-      },
-      {
-        title: "When code speaks for itself, it's only fair to make it heard.",
-        description: `Automated summaries not only save time but also offer consistency, accuracy, and insight into projects, helping developers focus on coding rather than documentation.`,
-      },
-    ],
-  },
-];
+	{
+		title: "Connect to GitHub Repository",
+		steps: [
+			{
+				title: "Accessing the Codebase",
+				description:
+					"Link your GitHub repository to access and analyze the codebase for documentation generation.",
+			},
+			{
+				title: "Documentation begins with understanding the code",
+				description:
+					"Scans the repository to gather insights like the tech stack, dependencies, and directory structure.",
+			},
+		],
+	},
+	{
+		title: "Downloading and Analyzing the Codebase",
+		steps: [
+			{
+				title: "Fetching and Structuring Data",
+				description:
+					"Downloads and organizes code files to facilitate detailed analysis.",
+			},
+			{
+				title: "Code Parsing and Analysis",
+				description:
+					"Identifies key elements like classes, functions, and modules, building a comprehensive code understanding.",
+			},
+			{
+				title: "Every line of code has a story to tell",
+				description:
+					"Prepares summaries of the project’s purpose, functionality, and technical details.",
+			},
+		],
+	},
+	{
+		title: "Graph Creation for Visual Representation",
+		steps: [
+			{
+				title: "Generating Graphs and Visualizations",
+				description:
+					"Creates graphs illustrating relationships between files, modules, and functions.",
+			},
+			{
+				title: "Dependency and Hierarchical Graphs",
+				description:
+					"Shows the architectural hierarchy and interdependencies in the project.",
+			},
+			{
+				title: "Interactive and Customizable Visuals",
+				description:
+					"Allows users to customize and explore complex project structures interactively.",
+			},
+			{
+				title: "A picture is worth a thousand lines of code",
+				description:
+					"Helps developers quickly understand project structure through visual aids.",
+			},
+		],
+	},
+	{
+		title: "Summarizing the Code and Its Core Functions",
+		steps: [
+			{
+				title: "Extracting Key Functions and Purpose",
+				description:
+					"Analyzes and summarizes functions, modules, and classes with concise explanations.",
+			},
+			{
+				title: "Automated Summary for Core Project Aspects",
+				description:
+					"Summarizes key details like function names, parameters, and comments in an accessible format.",
+			},
+			{
+				title: "Integration with README Components",
+				description:
+					"Organizes summaries into README sections for cohesive project understanding.",
+			},
+			{
+				title: "When code speaks for itself, it's only fair to make it heard.",
+				description:
+					"Offers consistent and insightful summaries, saving time and enhancing project clarity.",
+			},
+		],
+	},
+]
